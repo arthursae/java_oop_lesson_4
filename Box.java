2fdd1ebe31fe655cbed16d26ca85d93cad65e308
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-21 21:47:32 +0500

    Update Box.java

--- Box.java
+++ Box.java
@@ -2,27 +2,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 // FIXME: 06.07.2023 Добавить нужный дженерик.
-public class Box<E extends Fruit> {
+public class Box<T extends Fruit> {
 
     // FIXME: 06.07.2023 Реализовать методы согласно заданию.
-    List<E> fruits;
+    List<T> fruits;
     public Box() {
         this.fruits = new ArrayList<>();
     }
 
-    public void add(E fruit) {
+    public void add(T fruit) {
         fruits.add(fruit);
     }
 
     public int getWeight() {
         int totalWeight = 0;
-        for (E fruit : fruits) {
+        for (T fruit : fruits) {
             totalWeight += fruit.getWeight();
         }
         return totalWeight;
     }
 
-    public void moveTo(Box<? super E> anotherBox) {
+    public void moveTo(Box<? super T> anotherBox) {
         anotherBox.fruits.addAll(this.fruits);
         this.fruits.clear();
     }
